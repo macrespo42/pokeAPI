cc5ae1e3ea15fcde49cf156a53f08053e78d90ac
macrespo42 <macrespo@student.42.fr>
2025-09-10 13:33:08 +0200

    feat: add REPL

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { startREPL } from "./repl.js";
+
 function main(): void {
-  console.log("Hello, world!");
+  startREPL();
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,7 +1,26 @@
+import process from "node:process";
+import * as readline from "node:readline";
+
 export function cleanInput(input: string): string[] {
   return input
     .trim()
     .toLowerCase()
     .split(" ")
     .filter((word) => word.length > 0);
+}
+
+export function startREPL(): void {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "pokedex> ",
+  });
+  rl.prompt();
+  rl.on("line", (rawInput) => {
+    const input = cleanInput(rawInput);
+    if (input.length > 0) {
+      console.log(`Your command was: ${input[0]}`);
+    }
+    rl.prompt();
+  });
 }
